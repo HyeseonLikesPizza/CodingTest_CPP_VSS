593593cb1af1131690c9083eb26417b20b0997fc
임혜선 <1725f0701@gmail.com>
2022-02-22 19:28:07 +0900

    self number 수정

--- CodingTest_CPP_VSS/CodingTest_CPP_VSS/셀프넘버_0222.cpp
+++ CodingTest_CPP_VSS/CodingTest_CPP_VSS/셀프넘버_0222.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+int SelfNumber(int num) {
+	int w = num, i;
+	string str = to_string(num);
+	
+	for (i = 0; i < str.size(); i++)
+	{
+		w += str[i]-'0';
+	}
+
+	return w;
+}
+
 int main() {
 
+	int num = 2, res=0, i, cnt=1;
+	printf("1\n");
+
+	
+	while (cnt <= 10000)
+	{
+		res = SelfNumber(num);
+
+		if(res != cnt)
+
+
+		for (i = num + 1; i < res; i++)
+			printf("%d\n", i);
+		
+		num = res;
+		cnt++;
+	}
+	
 
 	return 0;
 }
