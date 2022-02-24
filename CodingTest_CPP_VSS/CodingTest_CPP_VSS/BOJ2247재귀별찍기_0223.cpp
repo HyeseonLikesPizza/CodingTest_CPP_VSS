c406c122c659f72a89e67c0e36af2c53db1cea32
임혜선 <1725f0701@gmail.com>
2022-02-24 19:31:47 +0900

    재귀별찍기 수정(25일 다시 풀기!!)

---
+++ CodingTest_CPP_VSS/CodingTest_CPP_VSS/BOJ2247재귀별찍기_0223.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void RecursiveStar(int n, int cnt) 
+{
+	if (n == 1) {
+		printf("*");
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		RecursiveStar(n / 3, cnt++);
+	}
+
+	if (cnt / 3 == 0)
+		printf("\n");
+
+}
+
+int main() {
+
+	scanf("%s", &N);
+	RecursiveStar(N, 1);
+
+	return 0;
+}
