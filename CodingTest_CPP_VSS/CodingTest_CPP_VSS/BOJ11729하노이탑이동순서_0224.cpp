d01a4ed1125cc655c922a26eb932cb404edcea94
임혜선 <1725f0701@gmail.com>
2022-02-24 20:30:34 +0900

    하노이탑

---
+++ CodingTest_CPP_VSS/CodingTest_CPP_VSS/BOJ11729하노이탑이동순서_0224.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+bool rod[3] = { true, false, false };
+int cnt = 0;
+int K;
+
+void Hanoi(int i, int j, int k) {
+
+	
+	
+
+}
+
+int main() {
+
+	cin >> K;
+	
+	Hanoi(1, 3, K);
+
+	return 0;
+}
