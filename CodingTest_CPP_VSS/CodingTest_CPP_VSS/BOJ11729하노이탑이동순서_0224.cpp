f1d220a76aca2006e5c659c28b84f9a08a8be52c
임혜선 <1725f0701@gmail.com>
2022-02-25 16:43:37 +0900

    하노이탑 수정 (정답)

--- CodingTest_CPP_VSS/CodingTest_CPP_VSS/BOJ11729하노이탑이동순서_0224.cpp
+++ CodingTest_CPP_VSS/CodingTest_CPP_VSS/BOJ11729하노이탑이동순서_0224.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-bool rod[3] = { true, false, false };
-int cnt = 0;
-int K;
+int N;
 
-void Hanoi(int i, int j, int k) {
+void Hanoi(int moveFrom, int moveTo, int n) {
 
+
+	if (n == 1) {
+		printf("%d %d\n", moveFrom, moveTo);
+		return;
+	}
 	
-	
+	Hanoi(moveFrom, 6 - (moveFrom + moveTo), n - 1);
+	Hanoi(moveFrom, moveTo, 1);
+	Hanoi(6 - (moveFrom + moveTo), moveTo, n - 1);
 
 }
 
 int main() {
 
-	cin >> K;
+	cin >> N;
+	int res = pow(2, N) - 1;
 	
-	Hanoi(1, 3, K);
+	cout << res << endl;
+	Hanoi(1, 3, N);
 
 	return 0;
 }
