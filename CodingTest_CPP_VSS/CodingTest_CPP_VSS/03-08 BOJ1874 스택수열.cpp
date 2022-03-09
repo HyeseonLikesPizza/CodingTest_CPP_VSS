1e69b7d400d8d68a4250b3c561b0b71126d6e222
HyeSeon <1725f0701@gmail.com>
2022-03-09 15:05:27 +0900

    03/09 스택수열 못풀었음

--- CodingTest_CPP_VSS/CodingTest_CPP_VSS/03-08 BOJ1874 스택수열.cpp
+++ CodingTest_CPP_VSS/CodingTest_CPP_VSS/03-08 BOJ1874 스택수열.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable: 4996)
 #include <iostream>
 #include <stack>
 #include <vector>
@@ -5,25 +7,53 @@ using namespace std;
 
 int main() {
 
-	int n, num, arr[100000];
+	int n, num, cnt=1, i, top;
 	stack<int> st;
 	vector<char> v;
+	bool cal = false;
 
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	
+	for (i = 0; i < n; i++) {		
 		scanf("%d", &num);
-		arr[i] = num;
-		if (st.empty())
+
+		if (!st.empty())
+			top = st.top();
+		if (st.empty() || top < num)
 		{
-			for (int i = 1; i <= num; i++) {
-				st.push(i);
-				v.push_back('+');
+			if (cnt > num) {
+				cal = false;
+				break;
 			}
-			st.pop(num);
-			v.
+			while (cnt <= num) {
+				st.push(cnt);
+				v.push_back('+');
+				cnt++;
+			}
+			st.pop();
+			v.push_back('-');
+		}
+		else if (top >= num) {
+			int top = st.top();
+			for (int j = top; j >= num; j--) {
+				st.pop();
+				v.push_back('-');
+			}
 		}
 	}
 
+	if (i == n && st.empty())
+		cal = true;
+
+	if (cal) {
+		for (int i = 0; i < v.size(); i++) {
+			printf("%c\n", v[i]);
+		}
+	}
+	else
+	{
+		cout << "NO";
+	}
 
 
 	return 0;
